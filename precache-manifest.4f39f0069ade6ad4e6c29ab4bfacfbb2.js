f06153c45d34c9344415ec8e58e5573581be0371
slevinger <shir.levinger@gmail.com>
2020-05-13 17:10:29 +0300

    Updates

---
+++ precache-manifest.4f39f0069ade6ad4e6c29ab4bfacfbb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae37a28d664934db7ae6e31a0b43ed3",
+    "url": "/code-name-web/index.html"
+  },
+  {
+    "revision": "3a8445c0e4738132235c",
+    "url": "/code-name-web/static/css/main.52a6050f.chunk.css"
+  },
+  {
+    "revision": "262ab85e1be13c564b3f",
+    "url": "/code-name-web/static/js/2.ded3e262.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/code-name-web/static/js/2.ded3e262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a8445c0e4738132235c",
+    "url": "/code-name-web/static/js/main.83ba4fe9.chunk.js"
+  },
+  {
+    "revision": "ef2410954f2a9746be14",
+    "url": "/code-name-web/static/js/runtime-main.26c7024d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/code-name-web/static/media/logo.5d5d9eef.svg"
+  }
+]);
